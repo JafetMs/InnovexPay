8e0dc91557d35caf114b6bee1b9704414f53a1ec
JafetMs <alexcero_@hotmail.com>
2023-05-27 20:48:09 -0600

    Accounts separado

---
+++ js/accounts.js
@@ -0,0 +1,5 @@
+const accounts = [
+    { userName: 'Jafet', password:'123', balance: 1000},
+    { userName: 'Julia', password:'321', balance: 2000},
+    { userName: 'Charlotte', password:'devF', balance: 0},
+]
